7cf58c9605b637dfbd55ca4e649f04c28f31e3cc
yarosla7 <yanin.yarosla7@gmail.com>
2023-01-03 00:14:37 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,6 @@ import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args) {
-
-        String toCut = "коты";
-        toCut = toCut.replace(" ", "");
-        char[] twoWords = toCut.toCharArray();
-
-        for (int i = 0; i < twoWords.length; i++) {
-            if (i < twoWords.length / 2) {
-                System.out.print(twoWords[i]);
-            }
-        }
-        System.out.print(" ");
-        for (int i = 0; i < twoWords.length; i++) {
-            if (i >= twoWords.length / 2) {
-                System.out.print(twoWords[i]);
-            }
-        }
+        System.out.println("Hello world!");
     }
 }
