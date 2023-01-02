74aa60842d2c8bb4ffbd19f65827b23c15818a60
yarosla7 <yanin.yarosla7@gmail.com>
2023-01-03 00:16:09 +0300

    task done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@ import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String toCut = "коты";
+        toCut = toCut.replace(" ", "");
+        char[] twoWords = toCut.toCharArray();
+
+        for (int i = 0; i < twoWords.length; i++) {
+            if (i < twoWords.length / 2) {
+                System.out.print(twoWords[i]);
+            }
+        }
+        System.out.print(" ");
+        for (int i = 0; i < twoWords.length; i++) {
+            if (i >= twoWords.length / 2) {
+                System.out.print(twoWords[i]);
+            }
+        }
     }
 }
