186b7c9dcb6755fae936d6a1947a3b663403308d
yarosla7 <yanin.yarosla7@gmail.com>
2023-01-04 15:36:33 +0300

    убрал лишние операторы, перенес условие в цикл

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,12 @@ public class Main {
         toCut = toCut.replace(" ", "");
         char[] twoWords = toCut.toCharArray();
 
-        for (int i = 0; i < twoWords.length; i++) {
-            if (i < twoWords.length / 2) {
-                System.out.print(twoWords[i]);
-            }
+        for (int i = 0; i < twoWords.length / 2; i++) {
+            System.out.print(twoWords[i]);
         }
         System.out.print(" ");
-        for (int i = 0; i < twoWords.length; i++) {
-            if (i >= twoWords.length / 2) {
-                System.out.print(twoWords[i]);
-            }
+        for (int i = twoWords.length / 2; i <= twoWords.length - 1; i++) {
+            System.out.print(twoWords[i]);
         }
     }
 }
